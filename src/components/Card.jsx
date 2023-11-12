8845afedaa1a2307932c8de25259bebec5864920
basat <itzbasatmaqsood@gmail.com>
2023-11-12 15:12:22 +0500

    code refactor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,25 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Checkbox } from '@mui/material';
-import { Favorite, FavoriteBorder } from '@mui/icons-material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import ShareIcon from "@mui/icons-material/Share";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Checkbox } from "@mui/material";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
 
-
-export default function FeedCard() {
-
+export default function FeedCard({ image, title, subheader, description }) {
   return (
-    <Card sx={{sm:{margin:5}, margin:"1rem"}} >
+    <Card sx={{ sm: { margin: 5 }, margin: "1rem" }}>
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
+            B
           </Avatar>
         }
         action={
@@ -29,25 +27,23 @@ export default function FeedCard() {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={title}
+        subheader={subheader}
       />
       <CardMedia
         component="img"
         height="20%"
-        image="https://images.pexels.com/photos/134643/pexels-photo-134643.jpeg?auto=compress&cs=tinysrgb&w=400"
+        image={image}
         alt="Paella dish"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          {description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
-        <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+          <Checkbox icon={<FavoriteBorder  />} checkedIcon={<Favorite color="error" />} />
         </IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,13 +4,37 @@ import FeedCard from "./Card";
 
 export default function Feed() {
   return (
-    <Box flex={6} p={2} >
-      <FeedCard/>
-      <FeedCard/>
-      <FeedCard/>
-      <FeedCard/>
-      <FeedCard/>
-      <FeedCard/>
+    <Box flex={6} p={2}>
+      <FeedCard
+        image={
+          "https://images.pexels.com/photos/5608022/pexels-photo-5608022.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+        title={"Basat Maqsood"}
+        subheader={"November 13, 2023"}
+        description={
+          "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
+        }
+      />
+      <FeedCard
+        image={
+          "https://images.pexels.com/photos/1660603/pexels-photo-1660603.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+        title={"Basat Maqsood"}
+        subheader={"November 12, 2023"}
+        description={
+          "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
+        }
+      />
+      <FeedCard
+        image={
+          "https://images.pexels.com/photos/1473673/pexels-photo-1473673.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+        title={"Arbab Mustafa"}
+        subheader={"November 11, 2023"}
+        description={
+          "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
+        }
+      />
     </Box>
   );
 }
